0478c27de1438973abbef2d938395bc940cdc976
CodeJuan <CodeJuan@users.noreply.github.com>
2017-01-30 20:40:08 +0800

    Update build.sh

--- lxd/build.sh
+++ lxd/build.sh
@@ -1,1 +1,13 @@
 # build lxd on centos
+
+go get github.com/lxc/lxd
+cd $GOPATH/src/github.com/lxc/lxd
+make
+
+# but pkg-config failed
+# pkg-config --cflags lxc
+#Package lxc was not found in the pkg-config search path.
+#Perhaps you should add the directory containing `lxc.pc'
+#to the PKG_CONFIG_PATH environment variable
+find / -type f -name "lxc.pc"
+export PKG_CONFIG_PATH="/usr/lib/pkgconfig"
