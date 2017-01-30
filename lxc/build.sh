fd1acfc2a87c0841f14a30169fe8e67c95bde347
CodeJuan <CodeJuan@users.noreply.github.com>
2017-01-30 11:01:49 +0800

    Update build.sh

--- lxc/build.sh
+++ lxc/build.sh
@@ -1,4 +1,6 @@
 # build lxc on centos
+
+# 2.0.7   wget https://github.com/lxc/lxc/archive/lxc-2.0.7.zip
 git clone https://github.com/lxc/lxc.git
 
 # libtoolize
@@ -6,3 +8,6 @@ yum install -y libtool
 
 # autogen
 ./autogen.sh
+
+# ./configure
+./configure
